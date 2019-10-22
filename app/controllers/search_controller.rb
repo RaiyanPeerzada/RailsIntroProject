9e4b003b561be83ab9d40a8cfa61b94e197fdba0
RaiyanPeerzada <p_raiyan@outlook.com>
2019-10-22 17:36:43 -0500

    Search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
+
 class SearchController < ApplicationController
-  def index
-  end
+  def index; end
 
   def result
+    @universities = University.where('name LIKE ?', "%#{params[:q]}%").order(:name).page params[:page]
+    @uni_count = University.where('name LIKE ?', "%#{params[:q]}%")
   end
 
   def studentResult
+    @students = Student.where('fname LIKE ? OR lname LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order(:fname).page params[:page]
+    @student_count = Student.where('fname LIKE ? OR lname LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%")
   end
 end
